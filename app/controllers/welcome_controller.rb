d80bba342da81ce315418f7a6c739419902f953e
unknown <>
2016-01-22 21:14:26 +0800

    Only one page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,7 @@
 class WelcomeController < ApplicationController
 	before_action :authenticate_user!,:except => [:index]
 	def index
-		if user_signed_in?  && current_user.email == 'hhhhbk605088@gmail.com' 
-			render :action => :sign
-		else if user_signed_in?
-			render :action => :unsign
-		end	
-		end
+	
 	end
 
 	def sign
